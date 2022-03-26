38e7c5f9c023791898338a1fb896079888eb3319
yubin21 <80163835+yubin21@users.noreply.github.com>
2022-03-26 23:10:11 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+		System.out.println(name+"??!");
+	}
+
+}
