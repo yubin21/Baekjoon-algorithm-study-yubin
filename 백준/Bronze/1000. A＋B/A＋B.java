1b26ec7083d47366b55ab18d8531d69700f1c87a
yubin21 <80163835+yubin21@users.noreply.github.com>
2022-03-26 22:58:17 +0900

    [Bronze V] Title: A+B, Time: 116 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1, n2;
+		Scanner sc = new Scanner(System.in);
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		System.out.println(n1+n2);
+	}
+
+}
