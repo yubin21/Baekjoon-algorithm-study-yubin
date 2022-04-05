af3a44aa157fddb96fcb4ccc36a4f4dbb023bf9a
yubin21 <80163835+yubin21@users.noreply.github.com>
2022-04-05 23:13:05 +0900

    [Bronze III] Title: 구구단, Time: 112 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for (int i = 1; i < 10; i++)
+			System.out.println(num + " * " + i + " = " + num*i);
+		
+	}
+
+}
+//백준 2739번.
